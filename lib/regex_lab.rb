201ccf8c0d35aebccefa86b611af16942efcc708
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-04-24 18:49:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if (/aeiou/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if scan.text(/["words_starting_with_un_and_ending_with_ing"]/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_five_letters_long(text)
@@ -11,9 +19,16 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z][:punct:]$/)
+    return true
+  else
+    return false
+end
 end
 
 def valid_phone_number?(phone)
-
+347-841-5209
+return true
+555-444-7689
+return false
 end
